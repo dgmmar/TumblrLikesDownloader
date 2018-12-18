f65f21380f8873acb95dabfdd772fb7a7deda9fc
dgmmar <45965034+dgmmar@users.noreply.github.com>
2018-12-18 10:44:47 +0100

    Update tumblr_keys.py

--- tumblr_keys.py
+++ tumblr_keys.py
@@ -1,4 +1,4 @@
-consumer_key = ''
-consumer_secret = ''
+consumer_key = 'jlUA6q8UYwahhOSXnOXwCHNnJBS4kDaQyTVqtx6T1BIBvlIR17'
+consumer_secret = '5YRaqBv8YghwBEEzERARvkRoCRjPafEuUx8baNVFyUzhGc1czz'
 token_key = '' 
 token_secret = ''
